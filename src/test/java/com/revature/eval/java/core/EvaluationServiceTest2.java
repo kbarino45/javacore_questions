774a1f08f5d317561a25ad9f673c58a28bdb8602
kbarino45 <kainalu.barino@revature.net>
2022-01-26 15:21:16 -0700

    Local folder

---
+++ src/test/java/com/revature/eval/java/core/EvaluationServiceTest2.java
@@ -0,0 +1,60 @@
+package com.revature.eval.java.core;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class EvaluationServiceTest2 {
+
+	private static final EvaluationService evaluationService = new EvaluationService();
+
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+
+	/*******************************************************************
+	 * Question 2
+	 ******************************************************************/
+	@Test
+	public void basic() {
+		final String phrase = "Portable Network Graphics";
+		final String expected = "PNG";
+		assertEquals(expected, evaluationService.acronym(phrase));
+	}
+
+	@Test
+	public void punctuation() {
+		final String phrase = "First In, First Out";
+		final String expected = "FIFO";
+		assertEquals(expected, evaluationService.acronym(phrase));
+	}
+
+	@Test
+	public void NonAcronymAllCapsWord() {
+		final String phrase = "GNU Image Manipulation Program";
+		final String expected = "GIMP";
+		assertEquals(expected, evaluationService.acronym(phrase));
+	}
+
+	@Test
+	public void punctuationWithoutWhitespace() {
+		final String phrase = "Complementary metal-oxide semiconductor";
+		final String expected = "CMOS";
+		assertEquals(expected, evaluationService.acronym(phrase));
+	}
+
+}
